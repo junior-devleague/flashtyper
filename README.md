d68ca85740da4e61f5526cb6abce06a71a155a95
Gina W <watanabegina26@gmail.com>
2017-12-08 16:08:13 -1000

    Added data and gif files.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flashtyper
+
+![Final Product Example](https://github.com/junior-devleague/flashtyper/blob/master/example.gif)
---
+++ data.js
@@ -0,0 +1,149 @@
+
+
+var note1 = {
+  language: "HTML",
+  category: "element",
+  description: "The div tag defines a division or a section in an HTML document"
+}
+
+var note2 = {
+  language: "JavaScript",
+  category: "syntax",
+  description: "JavaScript syntax is the set of rules how JavaScript programs are constructed"
+}
+
+var note3 = {
+  language: "JavaScript",
+  category: "variable",
+  description: "In a programming language variables are used to store data values"
+}
+
+var note4 = {
+  language: "JavaScript",
+  category: "variable",
+  description: "An equal sign is used to assign values to variables"
+}
+
+var note5 = {
+  language: "JavaScript",
+  category: "general",
+  description: "JavaScript statements are executed one by one in the same order as they are written"
+}
+
+var note6 = {
+  language: "JavaScript",
+  category: "syntax",
+  description: "Semicolons separate JavaScript statements"
+}
+
+var note7 = {
+  language: "JavaScript",
+  category: "syntax",
+  description: "Add a semicolon at the end of each executable statement"
+}
+
+var note8 = {
+  language: "JavaScript",
+  category: "practice",
+  description: "JavaScript ignores multiple spaces You can add white space to your script to make it more readable"
+}
+
+var note9 = {
+  language: "JavaScript",
+  category: "practice",
+  description: "For best readability programmers often like to avoid code lines longer than 80 characters"
+}
+
+var note10 = {
+  language: "JavaScript",
+  category: "practice",
+  description: "JavaScript comments can be used to explain JavaScript code and to make it more readable"
+}
+
+var note11 = {
+  language: "JavaScript",
+  category: "practice",
+  description: "JavaScript comments can also be used to prevent execution when testing alternative code"
+}
+
+var note12 = {
+  language: "JavaScript",
+  category: "datatype",
+  description: "JavaScript variables can hold many data types numbers strings objects and more"
+}
+
+var note13  = {
+  language: "JavaScript",
+  category: "datatype",
+  description: "Strings are written with quotes You can use single or double quotes"
+}
+
+var note14 = {
+  language: "JavaScript",
+  category: "datatype",
+  description: "Booleans can only have two values true or false"
+}
+
+var note15 = {
+  language: "JavaScript",
+  category: "datatype",
+  description: "JavaScript arrays are written with square brackets"
+}
+
+var note16 = {
+  language: "JavaScript",
+  category: "functions",
+  description: "A JavaScript function is executed when something invokes it or calls it"
+}
+
+var note17 = {
+  language: "JavaScript",
+  category: "datatype",
+  description: "The name value pairs in JavaScript objects are called properties"
+}
+
+var note18 = {
+  language: "JavaScript",
+  category: "scope",
+  description: "In JavaScript there are two types of scope local and global scope"
+}
+
+var note19 = {
+  language: "JavaScript",
+  category: "scope",
+  description: "A variable declared outside a function becomes GLOBAL"
+}
+
+var note20 = {
+  language: "JavaScript",
+  category: "scope",
+  description: "Variables declared within a JavaScript function become LOCAL to the function they can only be accessed within the function"
+}
+
+var note21 = {
+  language: "JavaScript",
+  category: "scope",
+  description: "If you assign a value to a variable that has not been declared it will automatically become a GLOBAL variable"
+}
+
+var note22 = {
+  language: "JavaScript",
+  category: "events",
+  description: "HTML events are things that happen to HTML elements When JavaScript is used in HTML pages JavaScript can react on these events"
+}
+
+var note23 = {
+  language: "JavaScript",
+  category: "math",
+  description: "The JavaScript Math object allows you to perform mathematical tasks on numbers"
+}
+
+var note24 = {
+  language: "JavaScript",
+  category: "switch",
+  description: "The JavaScript switch statement is used to perform different actions based on different conditions"
+}
+
+var terms = [note1, note2, note3, note4, note5, note6, note7, note8, note9, note10,
+note11, note12, note13, note14, note15, note16, note17, note18, note19, note20, note21,
+note22, note23, note24];
