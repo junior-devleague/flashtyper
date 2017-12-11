d9b1def2373a001b04c38126eac2bb804830843e
Jay Houn <jayhoun@gmail.com>
2017-12-11 12:16:19 -1000

    Update data.js

--- data.js
+++ data.js
@@ -147,5 +147,3 @@ var card24 = {
 var terms = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10,
 card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21,
 card22, card23, card24];
-
-// test
